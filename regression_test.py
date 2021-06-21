98b46362d95800970c5656acf2c66ed60780d32f
hoakw <hoakw@naver.com>
2021-06-21 15:26:59 +0900

    DAG_TEST_v0.1

---
+++ regression_test.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+import pymysql
+
+from sklearn import linear_model
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+args = {
+        'owner' : 'airflow',
+}
+
+with DAG(
+        dag_id='regression_test',
+        default_args=args,
+        schedule_interval=None,
+        start_date=days_ago(2),
+        tags=['modeling'],
+) as dag:
+
+    def get_result():
+        alpha = 0.0
+        beta = 0.0
+        intercept = 0.0
+
+        data_dict = {}
+
+        conn = pymysql.connect(host='192.168.100.104',
+                                user='june',
+                                password='wns123',
+                                db='ml_test',
+                                charset='utf8',
+                                cursorclass=pymysql.cursors.DictCursor)
+        sql = conn.cursor()
+
+        sql_query = "select * from ml_test"
+
+        sql.execute(sql_query)
+
+        rows = sql.fetchall()
+        df = pd.DataFrame(rows)
+
+        print("#####[JUNE]#####")
+        print("table = \n", df)
+
+        X = df[['cpu', 'mem']]
+        y = df['power']
+
+
+        linear_regression = linear_model.LinearRegression(fit_intercept=True)
+        linear_regression.fit(X=pd.DataFrame(X), y=y)
+
+        print("#####[JUNE] a = ", linear_regression.coef_[0])
+        print("#####[JUNE] b = ", linear_regression.coef_[1])
+        print("#####[JUNE] intercept = ", linear_regression.intercept_)
+
+        sql.close()
+
+    run_this = PythonOperator(
+            task_id = 'modeling',
+            python_callable=get_result,
+    )
+
+        run_this
--- test.py
+++ test.py
@@ -1,80 +1,43 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+import numpy as np
+import pandas as pd
+import pymysql
 
-"""Example DAG demonstrating the usage of the BashOperator."""
+from sklearn import linear_model
 
-from datetime import timedelta
 
-from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.operators.dummy import DummyOperator
-from airflow.utils.dates import days_ago
+alpha = 0.0
+beta = 0.0
+intercept = 0.0
 
-args = {
-    'owner': 'airflow',
-}
+data_dict = {}
 
-with DAG(
-    dag_id='example_bash_operator',
-    default_args=args,
-    schedule_interval='0 0 * * *',
-    start_date=days_ago(2),
-    dagrun_timeout=timedelta(minutes=60),
-    tags=['example', 'example2'],
-    params={"example_key": "example_value"},
-) as dag:
+conn = pymysql.connect(host='192.168.100.104',
+                        user='june',
+                        password='wns123',
+                        db='ml_test',
+                        charset='utf8',
+                        cursorclass=pymysql.cursors.DictCursor)
+sql = conn.cursor()
 
-    run_this_last = DummyOperator(
-        task_id='run_this_last',
-    )
+sql_query = "select * from ml_test"
 
-    # [START howto_operator_bash]
-    run_this = BashOperator(
-        task_id='run_after_loop',
-        bash_command='echo 1',
-    )
-    # [END howto_operator_bash]
+sql.execute(sql_query)
 
-    run_this >> run_this_last
+rows = sql.fetchall()
+df = pd.DataFrame(rows)
 
-    for i in range(3):
-        task = BashOperator(
-            task_id='runme_' + str(i),
-            bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
-        )
-        task >> run_this
+print(df)
 
-    # [START howto_operator_bash_template]
-    also_run_this = BashOperator(
-        task_id='also_run_this',
-        bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
-    )
-    # [END howto_operator_bash_template]
-    also_run_this >> run_this_last
+X = df[['cpu', 'mem']]
+y = df['power']
 
-# [START howto_operator_bash_skip]
-this_will_skip = BashOperator(
-    task_id='this_will_skip',
-    bash_command='echo "hello world"; exit 99;',
-    dag=dag,
-)
-# [END howto_operator_bash_skip]
-this_will_skip >> run_this_last
 
-if __name__ == "__main__":
-    dag.cli()
+linear_regression = linear_model.LinearRegression(fit_intercept=True)
+linear_regression.fit(X=pd.DataFrame(X), y=y)
+
+print("a = ", linear_regression.coef_[0])
+print("b = ", linear_regression.coef_[1])
+print("intercept = ", linear_regression.intercept_)
+
+sql.close()
+
